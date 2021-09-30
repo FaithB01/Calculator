9cfbc5d2f5ae9143700c209a763a60b3d0eed155
Faith <fnbiketi@gmail.com>
2021-09-29 18:53:07 -0700

    initalization

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int num1;
+  int num2;
+
+  cout << "Enter first number:" << endl;
+  cin >> num1;
+  cout << "Enter second number:" << endl;
+  cin >> num2;
+
+  cout << "=" << num1 + num2 << endl;
+
+  return 0;
+}
